5d799fc7ba4b52d84afd010911b1610ce54a1380
rishabsingla5@gmail.com <rishabsingla5@gmail.com>
2023-11-25 12:56:51 +0530

    sendingResponse

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const http = require('http');
-const PORT=3000;
+const PORT=3003;
 const server = http.createServer(function (req,res){
-    console.log('hello World');
+    if(req.url==='/home')
+    {
+        res.write(' Welcome home');
+    }
+    else if(req.url==='/about')
+    {
+        res.write('Welcome to About Us page');
+    }
+    else if(req.url==='/node')
+    {
+        res.write(' Welcome to my Node Js project');
+    }
 
 });
 server.listen(PORT);
