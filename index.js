8a43b8a940c992dfe39bfc6e15612efda278f2f9
rishabsingla5@gmail.com <rishabsingla5@gmail.com>
2023-11-28 13:09:03 +0530

    handlingRequestAndRouting

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const http = require('http');
-const express = require('express');
+const express=require("express");
+const bodyParser = require('body-parser');
 const app=express();
-app.use((req,res,next)=>{
-    console.log("1st middlware");
-    next();
+const PORT=8085;
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/add-product',(req,res,next)=>{
+    res.send(`<html>
+    <head> <title> Add Product</title></head>
+    <body><form action="/product" method="POST"><input type="text" name="title"><br> <input type="number" name="size"><button type="submit">Add product</button></form></body>
+    </html>`)
+});
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+app.use('/',(req,res,next)=>{
+    res.send('<h1>hello world</h1>');
+});
 
-})
-app.use((req,res,next)=>{
-    console.log("2nd middlware");
-    res.send('<h1>Hello World</h1>');
-
-    
-})
-
-http.createServer(app).listen(8080); 
+app.listen(PORT,()=>{
+    console.log("server started");
+});
