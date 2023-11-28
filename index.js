1d268a1abc862f33f13d68cd6578f87b1d1b5591
rishabsingla5@gmail.com <rishabsingla5@gmail.com>
2023-11-28 17:04:44 +0530

    cleanUpCodeUsingRouter

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 const express=require("express");
 const bodyParser = require('body-parser');
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
 const app=express();
 const PORT=8085;
 app.use(bodyParser.urlencoded({extended:false}));
-app.use('/add-product',(req,res,next)=>{
-    res.send(`<html>
-    <head> <title> Add Product</title></head>
-    <body><form action="/product" method="POST"><input type="text" name="title"><br> <input type="number" name="size"><button type="submit">Add product</button></form></body>
-    </html>`)
-});
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-app.use('/',(req,res,next)=>{
-    res.send('<h1>hello world</h1>');
-});
-
+app.use("/admin",adminRouter);
+app.use('/shop',shopRouter);
+app.use((req,res)=>{
+    res.status(404).send("<h1>Page Not Found</h1>");
+})
 app.listen(PORT,()=>{
     console.log("server started");
 });
